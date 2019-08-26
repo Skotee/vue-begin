2eedca21d77cebf1024ac757611e620d206a2867
Radek <radexw1@gmail.com>
2019-08-26 16:15:56 +0200

    auth complete without logout

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,20 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
-
 import modules from './modules'
+import fetch from '../services/fetch'
+import {
+  SET_TOKEN
+} from './types/auth.types'
+import createAuthTokenPlugin from '../plugins/fetch-plugin'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   modules,
+  plugins: [
+    createAuthTokenPlugin(fetch, {
+      setTokenMutationType: SET_TOKEN
+    })
+  ],
   strict: process.env.NODE_ENV !== 'production',
 })
---
+++ src/store/types/auth.types.js
@@ -0,0 +1,16 @@
+export const AUTH_MODULE = 'auth'
+export const AUTH_USERNAME = 'username'
+export const AUTH_TOKEN = 'token'
+
+export const GET_USERNAME = 'GET_USERNAME'
+
+export const IS_LOGGED = 'isLogged'
+
+export const SET_TOKEN = 'SET_TOKEN'
+
+export const SET_USERNAME = 'GET_USERNAME'
+
+export const ACTION_LOGIN = 'ACTION_LOGIN'
+
+export const ACTION_LOGOUT = 'ACTION_LOGOUT'
+
